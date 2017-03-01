f119b738dd7e46b26fd6753807f6ab322c899720
Victor F <xv1t@yandex.ru>
2017-03-01 16:17:41 +0400

    Create libreoffice.sh

---
+++ linux/ubuntu/libreoffice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# LibreOffice
+#
+
+
+sudo apt-get install libreoffice-style-tango libreoffice-gtk libreoffice-gtk3
+sudo apt-get install libreoffice-style-sifr
+sudo apt-get install libreoffice-calc
+sudo apt-get install libreoffice-writer
