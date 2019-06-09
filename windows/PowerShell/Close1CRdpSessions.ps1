fd0ef4133b8f97e341bd3cb3c74ac9ddfa9bbff5
Victor F <xv1t@yandex.ru>
2019-06-09 16:13:44 +0400

    Update Close1CRdpSessions.ps1

--- windows/PowerShell/Close1CRdpSessions.ps1
+++ windows/PowerShell/Close1CRdpSessions.ps1
@@ -1,5 +1,5 @@
-/**
-*/
+# Завершить все сеансы пользователей, у которых запущен процесс 1С
+
 $1cv8 = Get-WMIObject win32_process | where {$_.ProcessName -eq "1cv8.exe"}
 foreach ($proc in $cv8)
 {
